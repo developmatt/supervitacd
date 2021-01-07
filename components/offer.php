6ce35104e93934ff8745ebf30f99e2b1f4a984df
Matheus Godoi <matheusasdev@gmail.com>
2021-01-07 20:52:04 -0300

    Finalizando index

--- components/offer.php
+++ components/offer.php
@@ -2,6 +2,6 @@
     <div class="wrapper">
       <h3 class="offer__description">Aproveite agora e receba o SUPERVITA KIDS no conforto de sua casa, um suplemento alimentar cheio de vitaminas e minerais para sua criança.</h3>
 
-      <a href="#" class="btn btn-large btn-yellow">QUERO MEU FILHO SAUDÁVEL</a>
+      <a href="#" class="btn btn-large btn-blue">QUERO MEU FILHO SAUDÁVEL</a>
     </div>
   </section>
