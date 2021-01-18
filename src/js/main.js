501c6f731a9f4808e009cff95e79af98bea97730
Matheus Godoi <matheusasdev@gmail.com>
2021-01-18 19:14:13 -0300

    Permitindo visualização do vídeo da home no mobile

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,15 @@ require('./comments');
 require('./doubts');
 require('./mobile-menu');
 
-var video_wrapper = $('.youtube-video-place');
-//  Check to see if youtube wrapper exists
-if(video_wrapper.length){
-// If user clicks on the video wrapper load the video.
-$('.play-youtube-video').on('click', function(){
-/* Dynamically inject the iframe on demand of the user.
- Pull the youtube url from the data attribute on the wrapper element. */
-video_wrapper.html('<iframe allowfullscreen frameborder="0" width="600" height="338" class="embed-responsive-item" src="' + video_wrapper.data('yt-url') + '"></iframe>');
-});
+if($(window).innerWidth() <= 650) {
+    resizeVideoAccordingScreenWidth();
+}
+
+$(window).resize(function() {
+    if($(window).innerWidth() > 650) return false;
+    resizeVideoAccordingScreenWidth();
+})
+
+function resizeVideoAccordingScreenWidth() {
+    $('.video-container iframe').attr('width', $(window).innerWidth() - 50);
 }
